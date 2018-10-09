d0115b8db59158f730fd60c2ba54c7a7797367ab
Dan Rosenbloom <drosenbloom@logicworks.net>
2018-10-09 10:28:10 -0400

    fix(scripts): disable puppet services, add log messages

--- scripts/install-puppet.sh
+++ scripts/install-puppet.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+echo 'Installing puppet agent'
 wget https://apt.puppetlabs.com/puppet5-release-stretch.deb
 sudo dpkg -i puppet5-release-stretch.deb
 sudo apt-get update
 sudo apt-get install puppet-agent
+
+echo 'Disabling puppet agent/mcollective services'
+sudo systemctl stop puppet
+sudo systemctl stop mcollective.service
+sudo systemctl disable puppet
+sudo systemctl disable mcollective.service
+
